91738089c46ae4e6470859069718a0e8dff9e907
Admir Husic <admirhusic91@gmail.com>
2020-03-07 20:25:46 +0100

    Added post page and some minor fixes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Navbar popover
 $(document).ready(function(){
     $('[data-toggle="notification-popover"]').popover();
   });
+
+ // Post otions modal
+ 
+ $('#post-modal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
